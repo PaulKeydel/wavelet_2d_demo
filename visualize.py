7461c6ae90b2abf77fef220b31dd9926f5186453
Paul Keydel <paul.keydel@posteo.de>
2024-09-21 14:09:09 +0200

    entropy added

--- visualize.py
+++ visualize.py
@@ -2,9 +2,24 @@ import sys
 from pathlib import Path
 import struct
 import numpy as np
+import math
 import matplotlib.pyplot as plt
 from pylab import cm
 from matplotlib.colors import SymLogNorm
+
+def entropy(message):
+    n_labels = len(message)
+    if n_labels <= 1:
+        return 0
+    value, counts = np.unique(message, return_counts=True)
+    probs = counts / n_labels
+    n_classes = len(value)
+    if n_classes <= 1:
+        return 0
+    ent = 0.
+    for i in probs:
+        ent -= i * math.log(i, 2)
+    return ent
 
 assert(len(sys.argv) == 3)
 width = int(sys.argv[1])
@@ -13,6 +28,7 @@ num_pixel = width * height
 
 orig_bytes = Path('orig.bin').read_bytes()
 orig_data = struct.unpack('i'*num_pixel, orig_bytes)
+entr_orig = entropy(orig_data)
 orig_data = np.reshape(orig_data, (width,height))
 
 reco_bytes = Path('reco.bin').read_bytes()
@@ -21,6 +37,7 @@ reco_data = np.reshape(reco_data, (width,height))
 
 coeff_bytes = Path('coeffs.bin').read_bytes()
 coeff_data = struct.unpack('i'*num_pixel, coeff_bytes)
+entr_coeff = entropy(coeff_data)
 coeff_data = np.reshape(coeff_data, (width,height))
 coeff_data = abs(coeff_data)
 
@@ -44,8 +61,7 @@ pcoeff = axs[1, 1].matshow(coeff_data, cmap=cm.gray_r, norm=SymLogNorm(linthresh
 fig.colorbar(pcoeff, ax=axs[1, 1])
 axs[1, 1].set_title('abs(quant coeff)')
 
-#pdiff = axs[1, 1].matshow(reco_data - orig_data, cmap=cm.gray_r)
-#fig.colorbar(pdiff, ax=axs[1, 1])
-#axs[1, 1].set_title('reco - orig')
+txt = "Entropy original image: " + "{:.3f}".format(entr_orig) + "   /   entropy transformed image: " + "{:.3f}".format(entr_coeff)
+plt.figtext(0.5, 0.01, txt, wrap=True, horizontalalignment='center', fontsize=12)
 
 plt.show()
